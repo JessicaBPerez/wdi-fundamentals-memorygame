49c897a76567e452ad975aa770a5fd0fb8870b94
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro.local>
2019-01-20 17:42:29 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User fliiped " + cardThree);
+
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardTwo);
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay);
+
+var cardTwo = cards[3]
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  	alert("You found a match!");
+  }
+  else {
+  	alert("Sorry, try again.")
+  }
+};
