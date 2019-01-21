4b8a46a2521caf3e6b529433ed884299208c6cab
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro.local>
2019-01-21 08:05:18 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,74 +1,23 @@
 
-//console.log("User flipped " + cardOne);
-//console.log("User flipped " + cardTwo);
-
-var cards = [ {
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
-},
-{
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-hearts.png"
-},
-{
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/king-of-diamonds.png"
-}
-];
-
+var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
-  }
+      alert("You found a match!");
+    }
+    else {
+      alert("Sorry, try again.")
+    }
 }
 
 var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId].rank)
-  cardsInPlay.push(cards[cardId].rank);
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
   if (cardsInPlay.length === 2) {
     checkForMatch();
-    console.log("User flipped " + cards[cardId].rank);
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
-  }
-};
-
+  };
+}
 
 flipCard(0);
 flipCard(2);
-
-
-
-
-/* var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardsInPlay);
-
-var cardTwo = cards[3]
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-  	alert("You found a match!");
-  }
-  else {
-  	alert("Sorry, try again.")
-  }
-}; */
